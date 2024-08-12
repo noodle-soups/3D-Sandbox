6e1a38f28af313683c05d5dac209701798a5abb7
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-12 21:16:28 +0100

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        movePlayer();
+    }
+
+    public void movePlayer()
+    {
+        Vector3 _movement = new Vector3(move.x, 0f, move.y);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_movement), 0.15f);
+        transform.Translate(_movement * speed * Time.deltaTime, Space.World);
+    }
+}
