535b99c3cd9874fce8cca358a59edfa5cc4cc39c
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-14 19:19:39 +0100

    commit

---
+++ Assets/ObsticleHit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleHit : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+}
