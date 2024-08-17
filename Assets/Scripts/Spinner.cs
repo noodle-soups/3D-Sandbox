757c391cdbad2abd15ea2b9eb73ac1cfe0ba32db
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-17 11:36:13 +0100

    commit

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    [SerializeField] private float xAngle = 0f;
+    [SerializeField] private float yAngle = 0f;
+    [SerializeField] private float zAngle = 0f;
+
+    void Update()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
