1c17fc0c93bf8b6cbc7b449e39e551f3475d9aa0
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-30 20:21:04 +0100

    commit

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
+    private float bulletTimer;
+    [SerializeField] private float bulletTimerDestroy;
 
+
+    void Update()
+    {
+        bulletTimer += Time.deltaTime;
+        Debug.Log(bulletTimer);
+
+        if (bulletTimer > bulletTimerDestroy)
+            Destroy(gameObject);
+    }
 
 }
