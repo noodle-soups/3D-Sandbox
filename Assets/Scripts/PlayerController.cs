267902dfc2422829d3bb6c61ff4ddb8fe1fe811c
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-31 14:24:24 +0100

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -14,11 +17,15 @@ public class PlayerController : MonoBehaviour
     [Header("Player Properties")]
     [SerializeField] private float playerSpeed = 10f;
     [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float controllerDeadZone = 0.1f;
+    [SerializeField] private float gamepadRotateSmoothing = 1000f;
 
     // variables
     private Vector2 movement;
     private Vector2 aim;
     private Vector3 playerVelocity;
+
+    [SerializeField] private bool isGamepad;
 
 
     void Awake()
@@ -61,14 +68,30 @@ public class PlayerController : MonoBehaviour
 
     void HandleRotation()
     {
-        Ray ray = Camera.main.ScreenPointToRay(aim);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayDistance;
-
-        if (groundPlane.Raycast(ray, out rayDistance))
+        if (isGamepad)
         {
-            Vector3 point = ray.GetPoint(rayDistance);
-            LookAt(point);
+            if (Mathf.Abs(aim.x) > controllerDeadZone || Mathf.Abs(aim.y) > controllerDeadZone)
+            {
+                Vector3 playerDirection = Vector3.right * aim.x + Vector3.forward * aim.y;
+
+                if (playerDirection.sqrMagnitude > 0.0f)
+                {
+                    Quaternion newRotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, gamepadRotateSmoothing * Time.deltaTime);
+                }
+            }
+        }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(aim);
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayDistance;
+
+            if (groundPlane.Raycast(ray, out rayDistance))
+            {
+                Vector3 point = ray.GetPoint(rayDistance);
+                LookAt(point);
+            }
         }
     }
 
@@ -76,6 +99,11 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
         transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void OnDeviceChange(PlayerInput pi)
+    {
+        isGamepad = pi.currentControlScheme.Equals("Gamepad") ? true : false;
     }
 
 }
