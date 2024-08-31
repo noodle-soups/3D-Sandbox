712bdfeb88abf9cda0d8a2617651a32b728fb193
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-31 10:16:39 +0100

    commit

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,12 +8,31 @@ public class Gun : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
 
+    public float bulletCooldownTimer;
+    public float bulletCooldown = 1f;
+    public bool isBulletReady = true;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (isBulletReady && Input.GetKey(KeyCode.Space))
         {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             Debug.Log("Fire");
+            isBulletReady = false;
+
+            bulletCooldownTimer += Time.deltaTime;
         }
+
+        if (!isBulletReady)
+        {
+            bulletCooldownTimer += Time.deltaTime;
+            if (bulletCooldownTimer > bulletCooldown)
+            {
+                isBulletReady = true;
+                bulletCooldownTimer = 0f;
+            }
+        }
+
     }
 }
