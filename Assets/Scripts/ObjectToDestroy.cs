b94c92f99c028bcf3829bfbd6117bd88a05b632f
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-09-03 20:44:49 +0100

    commit

--- Assets/Scripts/ObjectToDestroy.cs
+++ Assets/Scripts/ObjectToDestroy.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class ObjectToDestroy : MonoBehaviour
 {
 
+    [SerializeField] private bool readyToDestroy = false;
+    [SerializeField] private float destroyRotationSpeed;
+    [SerializeField] private float destroyShrinkSpeed;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player-Bullet")
         {
             Debug.Log("Hit by bullet");
+
+            readyToDestroy = true;
+
             // destroy game object
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            
             // destroy bullet
             Destroy(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (readyToDestroy)
+        {
+            
+            float _destroyRotationSpeed = destroyRotationSpeed * Time.deltaTime;
+            transform.Rotate(0, _destroyRotationSpeed, 0);
+
+            float _destroyShrinkSpeed = destroyShrinkSpeed * Time.deltaTime;
+            transform.localScale -= new Vector3(_destroyShrinkSpeed, _destroyShrinkSpeed, _destroyShrinkSpeed);
+
+            if (Mathf.Min(transform.localScale.x, transform.localScale.y, transform.localScale.z) <= 0)
+            {
+                Destroy(gameObject);
+            }
+
         }
     }
 
