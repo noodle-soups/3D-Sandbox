d659ac62767f856e879c8b640e4cf1bc438891f8
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-10-04 18:16:14 +0100

    commit

--- Assets/Scripts/SecondaryWeapon.cs
+++ Assets/Scripts/SecondaryWeapon.cs
@@ -1,21 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SecondaryWeapon : MonoBehaviour
 {
-
+    // references
     private PlayerControls playerControls;
 
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform bulletSpawnPoint;
-
     [SerializeField] private float bulletDestroyTime;
+
+    [SerializeField] private bool isWeaponAvailable = true;
+    [SerializeField] private float cooldownRemaining;
+    [SerializeField] private float cooldownTime;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
-        playerControls.Controls.SecondaryWeapon.performed += ctx => FireSecondaryWeapon();
+        playerControls.Controls.SecondaryWeapon.performed += ctx => TryFireSecondaryWeapon();
     }
 
     private void OnEnable()
@@ -23,19 +27,42 @@ public class SecondaryWeapon : MonoBehaviour
         playerControls.Enable();
     }
 
-
     private void OnDisable()
     {
         playerControls.Disable();
     }
 
+
+    private void TryFireSecondaryWeapon()
+    {
+        if (isWeaponAvailable)
+        {
+            FireSecondaryWeapon();
+            StartCoroutine(SecondaryWeaponCooldown());
+        }
+    }
+
     private void FireSecondaryWeapon()
     {
         Debug.Log("Fire secondary weapon");
-
         var _bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         _bullet.transform.SetParent(bulletSpawnPoint);
         Destroy(_bullet, bulletDestroyTime);
+    }
+
+    private IEnumerator SecondaryWeaponCooldown()
+    {
+        isWeaponAvailable = false;
+
+        cooldownRemaining = cooldownTime;
+
+        while (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+            yield return null;
+        }
+
+        isWeaponAvailable = true;
     }
 
 }
